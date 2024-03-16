1c1cf776aeec528c1982f4b854fbd1888b82a133
23003301 <163319012+23003301@users.noreply.github.com>
2024-03-16 18:18:44 +0200

    Add files via upload

---
+++ EPH1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eph1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author EPHRAIM MAANDA
+ */
+public class EPH1 {
+    public static void main(String[] args) {
+            Scanner values= new Scanner(System.in);
+    int NUMBER1;
+    int NUMBER2;
+    int DIFFERANCE;
+    System.out.println("ENTER YOUR INTIAL NUMBER");
+    NUMBER1 = values.nextInt();
+     System.out.println("ENTER YOUR FINAL NUMBER");
+     NUMBER2=values.nextInt();
+     
+     DIFFERANCE=NUMBER2-NUMBER1;
+     System.out.println("YOUR ANSWER IS"+DIFFERANCE);
+}
+}
