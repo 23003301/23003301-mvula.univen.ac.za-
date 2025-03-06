2348e5d0c53a2cbd8084f19da9217d6b8b3661b0
23003301 <163319012+23003301@users.noreply.github.com>
2025-03-06 22:26:12 +0200

    Add files via upload

---
+++ Areaandperimeterofrectangle.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.areaandperimeterofrectangle;
+
+/**
+ *
+ * @author andan
+ */
+public class Areaandperimeterofrectangle {
+
+    public static void main(String[] args) {
+        
+        double base=10 ;
+        double height=4.5 ;
+        double Area;
+        
+        
+        
+        Area=0.5*base*height;
+        System.out.println("Area of a triangle is"+Area);
+        
+        
+        
+        
+    }
+}
